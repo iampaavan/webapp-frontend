a0501bccafbba28c7b2201e2f7b8da2c9fdcfa31
iampaavan <gopalareddy.p@husky.neu.edu>
2020-02-15 16:25:37 -0500

    shell command

--- k8s/change_imagetag.sh
+++ k8s/change_imagetag.sh
@@ -6,7 +6,7 @@ read -p "Enter Your Image Name: "  image
 
 rev=$(git rev-parse HEAD)
 
-sed -i -e "s/username/$username/g" -e "s/docker/$image/g" -e "s/81750055651bbe6db78ac1828abd43144f08213e/$rev/g" rc.yaml
+sed -i -e "s/username/$username/g" -e "s/clock/$image/g" -e "s/81750055651bbe6db78ac1828abd43144f08213e/$rev/g" rc.yaml
 
 cd
 
