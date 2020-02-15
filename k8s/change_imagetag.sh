1248abe58e5a63ba8d82bdfe53ebec8d84579014
iampaavan <gopalareddy.p@husky.neu.edu>
2020-02-15 14:18:10 -0500

    .

--- k8s/change_imagetag.sh
+++ k8s/change_imagetag.sh
@@ -6,7 +6,7 @@ read -p "Enter Your Image Name: "  image
 
 rev=$(git rev-parse HEAD)
 
-sed -i -e "s/username/$username/g" -e "s/image/$image/g" -e "s/81750055651bbe6db78ac1828abd43144f08213e/$rev/g" rc.yaml
+sed -i -e "s/username/$username/g" -e "s/docker/$image/g" -e "s/81750055651bbe6db78ac1828abd43144f08213e/$rev/g" rc.yaml
 
 cd
 
@@ -16,5 +16,5 @@ base64=$(base64 config.json | tr -d \\n)
 
 cd
 
-cd ./csye7374/webapp-frontend/k8s
+cd ~/csye7374/webapp-frontend/k8s
 sed -i "s/secret/$base64/g" secrets.yaml
