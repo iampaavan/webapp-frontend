fb0efdd548538607ffe575823d5c31f8b6b9b192
Hemal Gadhiya <gadhiya.h@husky.neu.edu>
2020-02-20 14:26:55 -0500

    ta demo

--- recipes/src/app/recipe.service.ts
+++ recipes/src/app/recipe.service.ts
@@ -1,26 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment} from "../environments/environment";
+import { environment} from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
 
-  url = `http://${process.env.API_URL}`
+  url = environment.url
 
   constructor(private http: HttpClient) { }
 
-  getRecipe():Observable<any>{
-    return this.http.get(this.url + '/v1/recipes')
+  getRecipe(): Observable<any> {
+    return this.http.get(this.url + '/v1/recipes');
   }
 
-  getRecipeById(id):Observable<any>{
-    return this.http.get(this.url+ '/v1/recipe/' + id)
+  getRecipeById(id): Observable<any> {
+    return this.http.get(this.url + '/v1/recipe/' + id);
   }
 
-  getRandomRecipe():Observable<any>{
-    return this.http.get(this.url + '/v1/get/random/recipe')
+  getRandomRecipe(): Observable<any> {
+    return this.http.get(this.url + '/v1/get/random/recipe');
   }
 }
