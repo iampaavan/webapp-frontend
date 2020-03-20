34e0b61dc8ab7ba89b8c84c020e9f81f96ab2a0d
iampaavan <gopalareddy.p@husky.neu.edu>
2020-03-20 15:27:36 -0400

    backedn url

--- recipes/src/environments/environment.ts
+++ recipes/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
 production: false,
-url: 'http://a74bdbed45a4e4a34a11dd91d885b20a-1705793997.us-east-1.elb.amazonaws.com'
+url: 'https://dev.paavangopala.me'
 };
 
 /*
