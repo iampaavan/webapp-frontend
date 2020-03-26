c82a4660506fc59523a99f77f68c5139f86d9522
iampaavan <gopalareddy.p@husky.neu.edu>
2020-03-26 11:33:46 -0400

    assignment 10

--- recipes/src/environments/environment.prod.ts
+++ recipes/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
 production: true,
-url: 'http://a809dc1480daa418dbd915e6aca1ef1a-1439064233.us-east-1.elb.amazonaws.com'
+url: 'http://aca0cbca630984e0aa546f1858d64621-987881237.us-east-1.elb.amazonaws.com'
 };
